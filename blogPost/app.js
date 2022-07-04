c44f54a0a82a158b13a1ee601e957c13206ed921
Mihir Yadav <72518307+mihir1729@users.noreply.github.com>
2022-07-04 14:30:30 +0530

    scroll-event

--- blogPost/app.js
+++ blogPost/app.js
@@ -1,3 +1,4 @@
+//featured projects button
 const slides = document.querySelectorAll(`.slide`);
 const nextBtn = document.querySelector(`.nextBtn`);
 const prevBtn = document.querySelector(`.prevBtn`);
@@ -30,3 +31,25 @@ function corousel() {
     slide.style.transform = `translateX(-${counter * 100}%)`;
   });
 }
+
+//fixing navbar
+const navbar = document.querySelector(`.header`);
+const links = document.querySelector(`.links`).querySelectorAll(`a`);
+
+window.addEventListener(`scroll`, function () {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  console.log(scrollHeight, navHeight);
+  if (scrollHeight > 1.5 * navHeight) {
+    navbar.classList.add(`fixed-nav`);
+    links.forEach(function (link) {
+      link.style.color = `#d6fff2`;
+    });
+  } else {
+    navbar.classList.remove(`fixed-nav`);
+
+    links.forEach(function (link) {
+      link.style.color = `black`;
+    });
+  }
+});
